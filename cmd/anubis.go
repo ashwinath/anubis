7cd9432ed516a95908074b00e351977346ec81a4
Ashwin Chatterji <ashwinath@hotmail.com>
2024-02-07 22:45:24 +0800

    Add dotfiles symlinks.

--- cmd/anubis.go
+++ cmd/anubis.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/ashwinath/anubis/pkg/common"
 	"github.com/ashwinath/anubis/pkg/config"
+	"github.com/ashwinath/anubis/pkg/linux"
 	"github.com/ashwinath/anubis/pkg/logger"
 )
 
@@ -20,62 +21,67 @@ func main() {
 	}
 
 	switch *target {
-	case "fedora-server":
-		fedoraServer(c)
+	case "fedora":
+		fedora(c)
 	default:
 	}
 }
 
-func fedoraServer(c *config.Config) {
+func fedora(c *config.Config) {
 	logger.Infof("begin process for fedora server")
 
-	//err := linux.InstallFedoraPackages(c.FedoraServer.DNF.Packages)
-	//if err != nil {
-	//logger.Errorf("error installing fedora packages, error: %v", err)
-	//}
+	err := linux.InstallFedoraPackages(c.FedoraServer.DNF.Packages)
+	if err != nil {
+		logger.Errorf("error installing fedora packages, error: %v", err)
+	}
 
-	err := common.CloneDotfiles(c.Dotfiles.Repo.HTTP, c.Dotfiles.Repo.SSH, false)
+	err = common.CloneDotfiles(c.Dotfiles.Repo.HTTP, c.Dotfiles.Repo.SSH, false)
 	if err != nil {
 		logger.Errorf("error cloning dotfiles, error: %v", err)
 	}
 
-	//err = linux.Chsh()
-	//if err != nil {
-	//logger.Errorf("could not change shell, error: %v", err)
-	//}
+	err = common.ConfigureDotFiles(c.FedoraServer.DotFiles, false)
+	if err != nil {
+		logger.Errorf("error cloning dotfiles, error: %v", err)
+	}
 
-	//err = linux.InstallFedoraRpms(c.FedoraServer.DNF.RPM)
-	//if err != nil {
-	//logger.Errorf("error installing fedora rpms, error: %v", err)
-	//}
+	err = linux.Chsh()
+	if err != nil {
+		logger.Errorf("could not change shell, error: %v", err)
+	}
 
-	//err = linux.FSTab(c.FedoraServer.FSTab)
-	//if err != nil {
-	//logger.Errorf("error editing /etc/fstab, error: %v", err)
-	//}
+	err = linux.InstallFedoraRpms(c.FedoraServer.DNF.RPM)
+	if err != nil {
+		logger.Errorf("error installing fedora rpms, error: %v", err)
+	}
 
-	//err = common.Pip(c.FedoraServer.Python.Packages)
-	//if err != nil {
-	//logger.Errorf("error running pip, error: %v", err)
-	//}
+	err = linux.FSTab(c.FedoraServer.FSTab)
+	if err != nil {
+		logger.Errorf("error editing /etc/fstab, error: %v", err)
+	}
 
-	//err = common.SSHAuthorizedKeys(c.FedoraServer.SSHPublicKeys, false)
-	//if err != nil {
-	//logger.Errorf("error running pip, error: %v", err)
-	//}
+	err = common.Pip(c.FedoraServer.Python.Packages)
+	if err != nil {
+		logger.Errorf("error running pip, error: %v", err)
+	}
 
-	//err = common.HardenSSHDaemon()
-	//if err != nil {
-	//logger.Errorf("error hardening sshd, error: %v", err)
-	//}
+	err = common.SSHAuthorizedKeys(c.FedoraServer.SSHPublicKeys, false)
+	if err != nil {
+		logger.Errorf("error running pip, error: %v", err)
+	}
 
-	//err = common.InstallBinaries(c.FedoraServer.Binaries)
-	//if err != nil {
-	//logger.Errorf("error installing binaries, error: %v", err)
-	//}
+	err = common.HardenSSHDaemon()
+	if err != nil {
+		logger.Errorf("error hardening sshd, error: %v", err)
+	}
 
-	//err = common.DownloadAndRunBinaries(c.FedoraServer.RunBinaries)
-	//if err != nil {
-	//logger.Errorf("error installing binaries, error: %v", err)
-	//}
+	err = common.InstallBinaries(c.FedoraServer.Binaries)
+	if err != nil {
+		logger.Errorf("error installing binaries, error: %v", err)
+	}
+
+	err = common.DownloadAndRunBinaries(c.FedoraServer.RunBinaries)
+	if err != nil {
+		logger.Errorf("error installing binaries, error: %v", err)
+	}
 }
--- pkg/common/dotfiles.go
+++ pkg/common/dotfiles.go
@@ -4,10 +4,16 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/ashwinath/anubis/pkg/logger"
 )
 
+const dotConfigFolderLinux = "/home/ashwin/.config"
+const dotConfigFolderDarwin = "/Users/ashwin/.config"
+const homeFolderLinux = "/home/ashwin"
+const homeFolderDarwin = "/Users/ashwin"
+const zshrcLine = "source ${HOME}/dotfiles/zsh/zshrc"
 const darwinDotFilesLocation = "/Users/ashwin/dotfiles"
 const linuxDotFilesLocation = "/home/ashwin/dotfiles"
 
@@ -51,5 +57,157 @@ func CloneDotfiles(gitURL string, gitSSHURL string, isDarwin bool) error {
 
 	logger.Infof("Done installing dotfiles")
 
+	return nil
+}
+
+func ConfigureDotFiles(items []string, isDarwin bool) error {
+	for _, item := range items {
+		switch item {
+		case "alacritty":
+			applyConfigStyleFolder(item, "alacritty.toml", isDarwin)
+		case "git":
+			applyHomeDotfilesStyle(item, []string{"gitignore", "gitconfig"}, isDarwin)
+		case "i3":
+			applyConfigStyleFolder(item, "config", isDarwin)
+		case "i3status-rust":
+			applyConfigStyleFolder(item, "config.toml", isDarwin)
+		case "nvim":
+			applyConfigStyleFolder(item, "init.vim", isDarwin)
+		case "ssh":
+			ssh(isDarwin)
+		case "tmux":
+			applyHomeDotfilesStyle(item, []string{"theme.tmux", "tmux.conf"}, isDarwin)
+		case "yabai":
+			// TODO when I sort this out
+		case "zsh":
+			zshrc(isDarwin)
+		}
+	}
+	return nil
+}
+
+func ssh(isDarwin bool) error {
+	logger.Infof("Install ssh config")
+
+	home := homeFolderLinux
+	if isDarwin {
+		home = homeFolderDarwin
+	}
+
+	dotfilesLoc := linuxDotFilesLocation
+	if isDarwin {
+		dotfilesLoc = darwinDotFilesLocation
+	}
+
+	os.Symlink(
+		fmt.Sprintf("%s/ssh/config", dotfilesLoc),
+		fmt.Sprintf("%s/.ssh/config", home),
+	)
+	os.Chown(fmt.Sprintf("%s/ssh/config", dotfilesLoc), 1000, 1000)
+
+	logger.Infof("Done install ssh config")
+	return nil
+}
+
+func zshrc(isDarwin bool) error {
+	logger.Infof("editing zshrc entry")
+
+	home := homeFolderLinux
+	if isDarwin {
+		home = homeFolderDarwin
+	}
+
+	zshrcLoc := fmt.Sprintf("%s/.zshrc", home)
+
+	zshrcData, err := os.ReadFile(zshrcLoc)
+	if err != nil {
+		return fmt.Errorf("could not read %s/.zshrc: %v", home, err)
+	}
+
+	found := false
+	for _, line := range strings.Split(string(zshrcData), newLine) {
+		if strings.Contains(line, zshrcLine) {
+			found = true
+			break
+		}
+
+	}
+	if found {
+		logger.Infof(".zshrc config is already updated, no op")
+		return nil
+	}
+
+	f, err := os.OpenFile(zshrcLoc, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+	if err != nil {
+		return fmt.Errorf("could not open %s for writing, %v", zshrcLoc, err)
+	}
+	defer f.Close()
+
+	if _, err := f.WriteString(zshrcLine); err != nil {
+		return fmt.Errorf("error writing to %s: %v", zshrcLoc, err)
+	}
+
+	logger.Infof("Finished editing .zshrc")
+
+	return nil
+}
+
+func applyHomeDotfilesStyle(configFolder string, items []string, isDarwin bool) error {
+	logger.Infof("Installing %v", items)
+
+	home := homeFolderLinux
+	if isDarwin {
+		home = homeFolderDarwin
+	}
+
+	dotfilesLoc := linuxDotFilesLocation
+	if isDarwin {
+		dotfilesLoc = darwinDotFilesLocation
+	}
+
+	for _, item := range items {
+		os.Symlink(
+			fmt.Sprintf("%s/%s/%s", dotfilesLoc, configFolder, item),
+			fmt.Sprintf("%s/.%s", home, item),
+		)
+		os.Chown(fmt.Sprintf("%s/.%s", home, item), 1000, 1000)
+	}
+
+	logger.Infof("Done installing %v", items)
+	return nil
+}
+
+func applyConfigStyleFolder(configFolder string, item string, isDarwin bool) error {
+	logger.Infof("Installing %s", configFolder)
+
+	loc := dotConfigFolderLinux
+	if isDarwin {
+		loc = dotConfigFolderDarwin
+	}
+
+	folder := fmt.Sprintf("%s/%s", loc, configFolder)
+	if _, err := os.Stat(folder); err != nil {
+		if err := os.MkdirAll(folder, 0755); err != nil {
+			return fmt.Errorf("could not mkdirall %s: %v", folder, err)
+		}
+	}
+
+	dotfilesLoc := linuxDotFilesLocation
+	if isDarwin {
+		dotfilesLoc = darwinDotFilesLocation
+	}
+
+	os.Symlink(
+		fmt.Sprintf("%s/%s/%s", dotfilesLoc, configFolder, item),
+		fmt.Sprintf("%s/%s/%s", loc, configFolder, item),
+	)
+
+	// change owner
+	out, err := exec.Command("chown", "-R", "1000:1000", folder).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("output: %s, error: %v", string(out), err)
+	}
+
+	logger.Infof("Done installing alacritty config")
 	return nil
 }
